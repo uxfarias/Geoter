960b099fe9700f254cbfbfb54373202650322078
uxfarias <vulgofariaspessoal@gmail.com>
2023-10-19 13:46:19 -0300

    commit pelo PC da empresa

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function () {
+    const parallaxBg = document.querySelector('.parallax-bg');
+    const scrolledValue = window.scrollY;
+
+    // Ajuste a velocidade de parallax modificando o valor abaixo
+    const parallaxSpeed = 0.6;
+
+    // Calcula a nova posição do plano de fundo
+    const newPosition = -scrolledValue * parallaxSpeed;
+
+    // Define a posição do plano de fundo
+    parallaxBg.style.transform = `translateY(${newPosition}px)`;
+});
